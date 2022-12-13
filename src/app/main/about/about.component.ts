a56da89a2a1b40f72f44f16c03bd9cf8996bcd6c
zvdas <rodriguezzude@gmail.com>
2022-12-13 09:48:31 +0530

    css style toggle switch added

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -13,29 +13,41 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  switchTheme(e: Event) {
-    // console.log(e.target);
-    console.log(e.target);
-
-    /*
-    :host {
-      --mainTextColor-light: #000;
-      --secondaryTextColor-light: rgb(51 51 51);
-      --mainLinkColor-light: #0da2b8;
-      --mainBorderColor-light: rgb(218, 218, 218);
-      --mainBgColor-light: rgb(249, 250, 251);
+  switchTheme(e: any) {
+    // console.log(e.target.checked);
+    if (e.target.checked) {
+      let hostStyle = document.documentElement.style;
+      hostStyle.setProperty('--mainTextColor-dark', '#fff');
+      hostStyle.setProperty('--secondaryTextColor-dark', '#adb0b1');
+      hostStyle.setProperty('--mainLinkColor-dark', 'rgb(30, 190, 214)');
+      hostStyle.setProperty('--mainBorderColor-dark', '#2b3031');
+      hostStyle.setProperty('--mainBgColor-dark', '#131415');
+      /*
+      :host {
+        --mainTextColor-dark: #fff;
+        --secondaryTextColor-dark: #adb0b1;
+        --mainLinkColor-dark: rgb(30, 190, 214);
+        --mainBorderColor-dark: #2b3031;
+        --mainBgColor-dark: #131415;
+      }
+      */
+    } else {
+      let hostStyle = document.documentElement.style;
+      hostStyle.setProperty('--mainTextColor-dark', '#000');
+      hostStyle.setProperty('--secondaryTextColor-dark', 'rgb(51 51 51)');
+      hostStyle.setProperty('--mainLinkColor-dark', '#0da2b8');
+      hostStyle.setProperty('--mainBorderColor-dark', 'rgb(218, 218, 218)');
+      hostStyle.setProperty('--mainBgColor-dark', 'rgb(249, 250, 251)');
+      /*
+      :host {
+        --mainTextColor-light: #000;
+        --secondaryTextColor-light: rgb(51 51 51);
+        --mainLinkColor-light: #0da2b8;
+        --mainBorderColor-light: rgb(218, 218, 218);
+        --mainBgColor-light: rgb(249, 250, 251);
+      }
+      */
     }
-    */
-
-    /*
-    :host {
-      --mainTextColor-dark: #fff;
-      --secondaryTextColor-dark: #adb0b1;
-      --mainLinkColor-dark: rgb(30, 190, 214);
-      --mainBorderColor-dark: #2b3031;
-      --mainBgColor-dark: #131415;
-    }
-    */
   }
 
   generatePdf() {
