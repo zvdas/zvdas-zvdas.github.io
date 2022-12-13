67251724fe4626fb0ee60bc2beb885bd00f86c5c
zvdas <rodriguezzude@gmail.com>
2022-12-13 09:21:02 +0530

    bootstrap toggle switch trial

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -13,6 +13,31 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  switchTheme(e: Event) {
+    // console.log(e.target);
+    console.log(e.target);
+
+    /*
+    :host {
+      --mainTextColor-light: #000;
+      --secondaryTextColor-light: rgb(51 51 51);
+      --mainLinkColor-light: #0da2b8;
+      --mainBorderColor-light: rgb(218, 218, 218);
+      --mainBgColor-light: rgb(249, 250, 251);
+    }
+    */
+
+    /*
+    :host {
+      --mainTextColor-dark: #fff;
+      --secondaryTextColor-dark: #adb0b1;
+      --mainLinkColor-dark: rgb(30, 190, 214);
+      --mainBorderColor-dark: #2b3031;
+      --mainBgColor-dark: #131415;
+    }
+    */
+  }
+
   generatePdf() {
     
   }
