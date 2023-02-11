fc34bb0724745a2fc09e378a67e6a8992e95feea
zvdas <rodriguezzude@gmail.com>
2023-02-11 16:53:00 +0530

    pipe removed

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TemplateComponent } from './template/template.component';
-import { SafeUrlPipe } from '../pipes/safe-url/safe-url.pipe';
 import { AboutComponent } from './about/about.component';
 import { ErrorComponent } from './error/error.component';
 
@@ -12,7 +11,6 @@ import { ErrorComponent } from './error/error.component';
     HomeComponent,
     NavbarComponent,
     TemplateComponent,
-    SafeUrlPipe,
     AboutComponent,
     ErrorComponent
   ],
