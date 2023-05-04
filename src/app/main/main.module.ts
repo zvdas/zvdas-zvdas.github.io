ceb0a6656f1f496f7c0368fe268fa3ae419e3ef5
zvdas <rodriguezzude@gmail.com>
2023-05-04 14:26:55 +0530

    chat api added

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,9 +7,6 @@ import { TemplateComponent } from './template/template.component';
 import { AboutComponent } from './about/about.component';
 import { ErrorComponent } from './error/error.component';
 import { MessagesComponent } from './messages/messages.component';
-
-// const  config: SocketIoConfig = { url: 'https://zvdas.github.io/', options: {} };
-const config: SocketIoConfig = { url: 'http://localhost:5000/', options: {} };
 
 @NgModule({
   declarations: [
@@ -23,7 +19,6 @@ const config: SocketIoConfig = { url: 'http://localhost:5000/', options: {} };
   ],
   imports: [
     CommonModule,
-    SocketIoModule.forRoot(config),
   ],
   exports: [
     NavbarComponent,
